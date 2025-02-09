f632f5798b600bcd1ef9382fa4dc59725a09bede
MarioAndreyHA <168862245+MarioAndreyHA@users.noreply.github.com>
2025-02-08 19:46:52 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if ("serviceWorker" in navigator) {
-      navigator.serviceWorker.register('/service-worker.js')
+      navigator.serviceWorker.register('/service-worker.js', {scope: './'})
         .then((registration) => {
           console.log('Service Worker registrado con éxito:', registration);
         })
